5d6e19a5a8c53a13cafe8a26744a44590ebe3539
Gianluca Guarini <gianluca.guarini@gmail.com>
2014-02-11 10:22:07 +0100

    v0.1.4

--- config/redaxo-backbone-requirejs/templates/include/end.inc.php
+++
@@ -1,5 +0,0 @@
-            <!-- build:js assets/js/build.min.js -->
-                <script data-main="assets/js/main" src="assets/vendor/requirejs/require.js"></script>
-            <!-- /build -->
-        </body>
-</html>
--- config/redaxo-backbone-requirejs/templates/main.php
+++
@@ -1,3 +0,0 @@
-<?php include_once('templates/include/start.inc.php'); ?>
-
-<?php include_once('templates/include/end.inc.php'); ?>
