1712f864b2aaeb565e7aa299b58b327271ce4be4
christianruhstaller <christian.ruhstaller@goldinteractive.ch>
2015-11-10 16:58:14 +0100

    deploy

--- tasks/deployment/deploy.sh
+++ tasks/deployment/deploy.sh
@@ -40,8 +40,6 @@ tar --exclude="$archiveFileName" -cvzf "$REV_FOLDER/$archiveFileName" -C "$REV_F
 
 mv "$REV_FOLDER" "$COPY_DEST"
 
-exit
-
 printf "Now we want to upload our archive to the server.\n"
 
 scp "$COPY_DEST/$archiveFileName" $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_APPROOT
