0d0f2aba225271554554b56ede84f4cdf10104f6
christianruhstaller <christian.ruhstaller@goldinteractive.ch>
2015-11-10 11:57:49 +0100

    clean

--- tasks/deployment/clean.sh
+++ tasks/deployment/clean.sh
@@ -4,6 +4,8 @@ source "$CONFIG_FOLDER/deployment"
 
 REV_FILE=$DEPLOYMENT_FOLDER/rev
 CURRENTREV=$(cat $REV_FILE)
+OLDREV=$(($CURRENTREV-1))
+OLDREV_FOLDER="rev$OLDREV"
 
 #load config
 DEPLOYMENT_CONFIG_FILE="$CONFIG_FOLDER/deployment.$DEPLOYENV"
@@ -13,3 +15,11 @@ if [ ! -e $DEPLOYMENT_CONFIG_FILE ]; then
 fi
 
 source $DEPLOYMENT_CONFIG_FILE
+
+ssh $DEPLOY_USER@$DEPLOY_HOST -p $DEPLOY_PORT   "$(which bash) -s" << EOF
+    cd $DEPLOY_APPROOT
+
+    mv $OLDREV_FOLDER "tmprev"
+    rm -rf rev*
+    mv "tmprev" $OLDREV_FOLDER
+EOF
