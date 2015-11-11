8b38db45b83efe8079d363c61495b64d07dcf5e8
christianruhstaller <christian.ruhstaller@goldinteractive.ch>
2015-11-11 11:09:34 +0100

    deployment setup

--- tasks/deployment/setup.sh
+++ tasks/deployment/setup.sh
@@ -12,3 +12,22 @@ source $DEPLOYMENT_CONFIG_FILE
 printf "Enter the SSH password for user ($DEPLOY_USER)\n"
 
 ssh-copy-id $DEPLOY_USER@$DEPLOY_HOST -p $DEPLOY_PORT
+
+
+if ! command -v mysql > /dev/null; then
+    echo -n "MySQL not found. Give the Path to MySQL (default: /Applications/MAMP/Library/bin/mysql) press [ENTER]: "
+    read MYSQL
+
+    rm -f /usr/local/bin/mysql
+    sudo ln -s $MYSQL /usr/local/bin/mysql
+fi
+
+
+if  ! command -v mysqldump  > /dev/null; then
+    echo -n "MySQLDump not found. Give the Path to MySQLDump (default: /Applications/MAMP/Library/bin/mysqldump) press [ENTER]: "
+    read MYSQLDUMP
+
+    rm -f /usr/local/bin/mysqldump
+    sudo ln -s $MYSQLDUMP /usr/local/bin/mysqldump
+fi
+
