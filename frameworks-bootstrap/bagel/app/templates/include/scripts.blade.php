c7ae17e3fb256e1540163038364564253172f9a1
Gianluca Guarini <gianluca.guarini@gmail.com>
2016-02-24 09:09:25 +0100

    updated: dev dependencies and bootstrap templates

--- frameworks-bootstrap/bagel/app/templates/include/scripts.blade.php
+++ frameworks-bootstrap/bagel/app/templates/include/scripts.blade.php
@@ -1,8 +1,6 @@
-<!-- build:js assets/js/main.min.js -->
-	<!-- everything is inside here will be removed, minified and replaced with a single script tag inside the dist/index.html file -->
-	<script src="assets/vendor/bower/jquery/dist/jquery.min.js"></script>
-	<script src="assets/js/main.js"></script>
-<!-- endbuild -->
+
+<script src="assets/js/main.bundle.js?@TIMESTAMP"></script>
+
 
 {% if not craft.request.isLivePreview %}
     <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
