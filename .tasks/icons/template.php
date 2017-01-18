27737fa1b9da55402d4e358abc61f261ce4aced7
Manuel Sommerhalder <hello@oncode.ch>
2017-01-18 16:11:06 +0100

    Prefix also last 2 versions of browsers

--- .tasks/icons/template.php
+++ .tasks/icons/template.php
@@ -1,4 +1,4 @@
-<svg class="hide" xmlns="http://www.w3.org/2000/svg">
+<svg class="hide grouped-svg" xmlns="http://www.w3.org/2000/svg">
     <defs>
         <?php foreach ($icons as $name => $content): ?>
             <g class="icon" id="<?php echo $name; ?>">
