a0dab3c8b07752d3f61b940204c0a8c545672e98
Manuel Sommerhalder <manuel.sommerhalder@goldinteractive.ch>
2017-09-27 18:49:46 +0200

    Update icon feature

--- .tasks/icons/template.php
+++ .tasks/icons/template.php
@@ -2,7 +2,8 @@
   class="grouped-svg"
   xmlns="http://www.w3.org/2000/svg"
   xmlns:xlink="http://www.w3.org/1999/xlink"
-  style="display: none;">
+  aria-hidden="true"
+  style="visibility: hidden; width: 0; height: 0; position: absolute;">
     <defs>
         <?php foreach ($icons as $name => $data): ?>
             <g class="icon -icon-<?php echo $name ?>" id="icon-<?php echo $name; ?>">
