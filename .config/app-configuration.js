775fd3080ffaf54220073060c0075a41e8ba9865
Simon Stucki <simon.stucki@goldinteractive.ch>
2018-07-16 15:52:24 +0200

    Add config files

---
+++ .config/app-configuration.js
@@ -0,0 +1,6 @@
+module.exports = () => ({
+  // configure css entry points, convention: 'style' will be resolved to 'css/style.scss'
+  cssEntries: [],
+  // configure js entry points, convention: 'index' will be resolved to 'js/index.js'
+  jsEntries: ['main']
+})
