53e3037d80cfb2d9a50688bee74fae1cb098230c
Simon Stucki <simon.stucki@goldinteractive.ch>
2018-07-19 23:10:36 +0200

    Add basic help

--- .scripts/help.sh
+++ .scripts/help.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
 set -eu
 
-echo "No help available, you're on your own."
+#!/bin/bash
+START="\033[35;1m"
+START2="\x1B[36m"
+END="\033[0m"
+
+printf "
+Command list:
+Work on the project:
+  $START  make docker-up        $END start the docker engine (this is required for all tasks)
+  $START  make docker-connect   $END connect to the docker container
+  $START  make install          $END install all project dependencies
+  $START  make watch            $END watch the js and the scss files compiling them
+  $START  make icons            $END generate the icons
+  $START  make favicons         $END generate the favicons
+
+Deploy the project:
+  $START  [git branches]        $END push to release/environment to deploy the project
+
+"
