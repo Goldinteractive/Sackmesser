2b1d3b8b66acc267a66219ab03d58779e17099b4
Simon Stucki <simon_stucki@hotmail.com>
2019-02-11 17:55:07 +0100

    Fix asset hash template replace path configuration
    Use dist only as part of post-build.sh script.
    Frontend build does not alter the dist structure.

--- .scripts/ci/post-build.sh
+++ .scripts/ci/post-build.sh
@@ -4,8 +4,8 @@ source .config/build
 source $SCRIPTS_FOLDER/util.sh
 
 # replace @ASSET_HASH in files of the following folder
-if [ ! -z $ASSET_HASH_TEMPLATE_REPLACE_PATH ]; then
-    egrep -lRZ "@ASSET_HASH" $ASSET_HASH_TEMPLATE_REPLACE_PATH | xargs --null sed -i "s/@ASSET_HASH/$ASSET_HASH/g"
+if [ ! -z $DEST/$ASSET_HASH_TEMPLATE_REPLACE_PATH ]; then
+    egrep -lRZ "@ASSET_HASH" $DEST/$ASSET_HASH_TEMPLATE_REPLACE_PATH | xargs --null sed -i "s/@ASSET_HASH/$ASSET_HASH/g"
 fi
 
 callRecipeEvent $RECIPE "build.post"
