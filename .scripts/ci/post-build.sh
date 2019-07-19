669bb43eae08f3045fb47ce82838de4d156aba8f
Simon Stucki <simon_stucki@hotmail.com>
2019-07-19 10:50:18 +0200

    Fix asset hash replace path check
    Don't prefix with $dest

--- .scripts/ci/post-build.sh
+++ .scripts/ci/post-build.sh
@@ -4,7 +4,7 @@ source .config/build
 source $SCRIPTS_FOLDER/util.sh
 
 # replace @ASSET_HASH in files of the following folder
-if [ ! -z $DEST/$ASSET_HASH_TEMPLATE_REPLACE_PATH ]; then
+if [ ! -z $ASSET_HASH_TEMPLATE_REPLACE_PATH ]; then
     egrep -lRZ "@ASSET_HASH" $DEST/$ASSET_HASH_TEMPLATE_REPLACE_PATH | xargs --null sed -i "s/@ASSET_HASH/$ASSET_HASH/g"
 fi
 
