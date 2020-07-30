80e68b69707c1873875933c6abe3086efc05e5b4
Marc <marc.berli@windowslive.com>
2020-07-30 11:17:25 +0200

    Add support for async/await

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,19 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache.forever()
 
   const presets = ['@babel/preset-env']
   const plugins = [
     ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
+    '@babel/plugin-transform-runtime',
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-transform-classes',
     '@babel/plugin-proposal-optional-chaining',
     '@babel/plugin-proposal-nullish-coalescing-operator',
-    'babel-plugin-dev-expression'
+    'babel-plugin-dev-expression',
   ]
 
   return {
     presets,
-    plugins
+    plugins,
   }
 }
