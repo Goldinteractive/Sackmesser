6193a3d1024fe0dcb448f90939ea4bf29975ddc4
lukasNo1 <lukas.wegmueller@goldinteractive.ch>
2020-10-02 16:00:04 +0200

    Apply project config on craft 3 deploy

--- .scripts/recipes/craft3/golive.pre.sh
+++ .scripts/recipes/craft3/golive.pre.sh
@@ -11,5 +11,6 @@ executeSSH "ln -s ../../_data/storage $APPROOT/deploy/backend/storage"
 
 # Replace the placeholder with the db pw for the environment
 executeSSH "sed -i \"s/@DB_PASSWORD/$DB_PASSWORD/g\" $APPROOT/deploy/backend/config/db.php" > /dev/null
+executeSSH "APPENV=$ENV $DEPLOY_PHP_BINARY $APPROOT/deploy/backend/craft project-config/apply"
 executeSSH "APPENV=$ENV $DEPLOY_PHP_BINARY $APPROOT/deploy/backend/craft blitz/cache/clear"
 executeSSH "APPENV=$ENV $DEPLOY_PHP_BINARY $APPROOT/deploy/backend/craft crafter/cache/clear-template"
