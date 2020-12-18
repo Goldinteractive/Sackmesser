f033092d0c02bad52c939d4224ed792a01eb3d6d
Christian Ruhstaller <christian.ruhstaller@goldinteractive.ch>
2020-12-18 13:08:31 +0100

    Fix icon template viewBox

--- .scripts/icons/template.php
+++ .scripts/icons/template.php
@@ -3,7 +3,7 @@
     xmlns="http://www.w3.org/2000/svg"
     xmlns:xlink="http://www.w3.org/1999/xlink">
     <?php foreach ($icons as $name => $data): ?>
-        <symbol class="icon -icon-<?php echo $name ?>" id="icon-<?php echo $name; ?>" viewBox=" <?php echo $data['viewBox']; ?>">
+        <symbol class="icon -icon-<?php echo $name ?>" id="icon-<?php echo $name; ?>" viewBox="<?php echo $data['attributes']['viewBox']; ?>">
             <?php echo $data['content']; ?>
         </symbol>
     <?php endforeach; ?>
