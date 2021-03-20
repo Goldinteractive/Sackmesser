740c66caf23d860185caf03587197130108b7960
Christian Ruhstaller <christian.ruhstaller@goldinteractive.ch>
2021-03-20 13:07:10 +0100

    Update icon handling

--- .scripts/icons/icons-compile.sh
+++ .scripts/icons/icons-compile.sh
@@ -8,5 +8,4 @@ source $SCRIPTS_FOLDER/util.sh
 php -f $SCRIPTS_FOLDER/icons/generate.php \
     sharedJson=$FE_SOURCE/shared-variables.json \
     iconsFolder=$ICONS_OUT \
-    dataFileOutput=$ICONS_DATA_FILE \
     svgCombOutput=$ICONS_COMBINED_SVG
--- .scripts/icons/template.php
+++ .scripts/icons/template.php
@@ -1,14 +1,10 @@
 <svg
-  class="grouped-svg"
-  xmlns="http://www.w3.org/2000/svg"
-  xmlns:xlink="http://www.w3.org/1999/xlink"
-  aria-hidden="true"
-  style="visibility: hidden; width: 0; height: 0; position: absolute;">
-    <defs>
-        <?php foreach ($icons as $name => $data): ?>
-            <g class="icon -icon-<?php echo $name ?>" id="icon-<?php echo $name; ?>">
-                <?php echo $data['content']; ?>
-            </g>
-        <?php endforeach; ?>
-    </defs>
+    class="grouped-svg"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink">
+    <?php foreach ($icons as $name => $data): ?>
+        <symbol class="icon -icon-<?php echo $name ?>" id="icon-<?php echo $name; ?>" viewBox="<?php echo $data['attributes']['viewBox']; ?>">
+            <?php echo $data['content']; ?>
+        </symbol>
+    <?php endforeach; ?>
 </svg>
