1e689eda9f507ef261c90aa1bf25a760d1c4dfe9
Ramon Schumacher <rschumacher.ch@gmail.com>
2022-04-05 12:44:45 +0200

    Update webpack config

--- .config/webpack/loader.js
+++ .config/webpack/loader.js
@@ -1,22 +1,24 @@
-const path = require('path');
-const paths = require('../paths');
+const path = require('path')
+const paths = require('../paths')
 const jsonImporter = require('node-sass-json-importer')
 
 const sassLoader = {
   loader: 'sass-loader',
   options: {
-    includePaths: [paths.app.cssBase, 'node_modules'],
-    importer: jsonImporter
-  }
+    sassOptions: {
+      includePaths: [paths.app.cssBase, 'node_modules'],
+      importer: jsonImporter,
+    },
+  },
 }
 
 const postcssLoader = {
   loader: 'postcss-loader',
   options: {
-    config: {
-      path: path.resolve('.config/postcss.config.js')
-    }
-  }
+    postcssOptions: {
+      config: path.resolve('.config/postcss.config.js'),
+    },
+  },
 }
 
 // Note that svg fonts won't be picked up by this loader
@@ -25,11 +27,10 @@ const fontLoader = () => ({
   use: {
     loader: 'file-loader',
     options: {
-      name: 'fonts/[name].[ext]'
-    }
-  }
+      name: 'fonts/[name].[ext]',
+    },
+  },
 })
-
 
 const imageLoader = ({ isProd, assetHash }) => ({
   test: /\.(gif|png|jpe?g|svg)$/i,
@@ -40,22 +41,22 @@ const imageLoader = ({ isProd, assetHash }) => ({
         name: isProd ? '[hash].[ext]' : '[path][name].[ext]',
         outputPath: '/files/',
         publicPath: assetHash + '/files/',
-        context: 'frontend'
-      }
+        context: 'frontend',
+      },
     },
     {
       loader: 'image-webpack-loader',
       options: {
         disable: true, // disabled in dev mode
         optipng: {
-          enabled: false
+          enabled: false,
         },
         pngquant: {
-          quality: '90-95'
-        }
-      }
-    }
-  ]
+          quality: '90-95',
+        },
+      },
+    },
+  ],
 })
 
 module.exports = {
