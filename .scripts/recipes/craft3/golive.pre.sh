cc8d70068a1e0a5bb95f6ada738bdffc93ad144b
lukasNo1 <lukas.wegmueller@goldinteractive.ch>
2022-04-28 14:42:48 +0200

    Clear craft 3 caches after deploy rather than before

--- .scripts/recipes/craft3/golive.pre.sh
+++ .scripts/recipes/craft3/golive.pre.sh
@@ -13,5 +13,3 @@ executeSSH "ln -s ../../_data/storage $APPROOT/deploy/backend/storage"
 executeSSH "sed -i \"s/@DB_PASSWORD/$DB_PASSWORD/g\" $APPROOT/deploy/backend/config/db.php" > /dev/null
 executeSSH "APPENV=$ENV $DEPLOY_PHP_BINARY $APPROOT/deploy/backend/craft migrate/all"
 executeSSH "APPENV=$ENV $DEPLOY_PHP_BINARY $APPROOT/deploy/backend/craft project-config/apply"
-executeSSH "APPENV=$ENV $DEPLOY_PHP_BINARY $APPROOT/deploy/backend/craft blitz/cache/clear"
-executeSSH "APPENV=$ENV $DEPLOY_PHP_BINARY $APPROOT/deploy/backend/craft crafter/cache/clear-template"
